6e4c8cd135b3a890154bafbf1cb37031dedaa913
Satyam <satyambhardawaj@gmail.com>
2020-08-28 14:22:55 +0530

    searchview complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,46 @@
-import axios from 'axios';
+import Search from './models/Search';
+import {elements, renderLoader, clearLoader} from './views/base';
+import * as searchView from './views/searchView';
 
-async function getResults(query) {
-    try{
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = res.data.recipes
-        console.log(recipes);
-    } catch(error){
-        alert(error)
+/** Global State of the app
+ * -Search object
+ * -current recipe object
+ * -liked recipe
+ */ 
+const state = {};
+
+const controlSearch = async () => {
+    // get the query from view
+    const query = searchView.getInput();
+
+    if (query){
+        // New search object and add to state
+        state.search = new Search(query)
+
+        // prepare UI for results
+        searchView.clearInput();
+        searchView.clearPrevResult();
+        renderLoader(elements.searchRes)
+
+        //Search for recipes
+        clearLoader();
+        await state.search.getResults();
+
+        // render results on UI
+        searchView.renderResults(state.search.result);
     }
-    
 }
 
-getResults('pizza');
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline')
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearPrevResult();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
